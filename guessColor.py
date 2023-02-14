f7ff32ba17dac7850f8b3bc392c74198ba93daf0
shaimaa <shemoorp@gmail.com>
2023-02-15 02:04:54 +0300

    project
    new

--- guessColor.py
+++
@@ -1,9 +0,0 @@
-color= "rr"
-if color == "red":
-   print("It is red")
-elif color == "green":
-   print("Its green")
-elif color == "purple":
-   print("Its purple ")
-else:
-   print("I havent heard of the color", color)
