6552e25938eae71e00a152227b2256ef85caa112
shaimaa <shemoorp@gmail.com>
2023-02-15 02:05:57 +0300

    Revert "project"
    nwe2

---
+++ guessColor.py
@@ -0,0 +1,9 @@
+color= "rr"
+if color == "red":
+   print("It is red")
+elif color == "green":
+   print("Its green")
+elif color == "purple":
+   print("Its purple ")
+else:
+   print("I havent heard of the color", color)
--- pythonFile.py
+++ pythonFile.py
@@ -1,6 +1,1 @@
-<html>
-
-<h2> hello</h2>
-
-
 print(" Here  is some Python code ")
